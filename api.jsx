17fddf083f020a0b1fdbeb6b937b851f666a09c3
Balaji <balaji.v@cannyfore.com>
2025-05-14 10:39:39 +0530

    new todo api is added

--- api.jsx
+++ api.jsx
@@ -2,6 +2,7 @@
 // export const API = "http://localhost:5000/auth";
 export const API_BASE = "https://todolist-backend-t4o7.onrender.com/api/auth";
 export const API = "https://todolist-backend-t4o7.onrender.com/auth";
+export const TODO_API = "https://todolist-backend-t4o7.onrender.com/todos";
 
 
     
