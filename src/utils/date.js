00b311e8621f5154304e01e291a79f467fdf65a0
Balaji <balaji.v@cannyfore.com>
2025-05-19 11:53:19 +0530

    date alone

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,15 @@
-export const formatToIST = (isoDateString) => {
-    return new Date(isoDateString).toLocaleString("en-IN", {
-      timeZone: "Asia/Kolkata",
-      hour12: true,
-    });
-  };
+// export const formatToIST = (isoDateString) => {
+//     return new Date(isoDateString).toLocaleString("en-IN", {
+//       timeZone: "Asia/Kolkata",
+//       hour12: true,
+//     });
+//   };
   
+export const formatToIST = (isoDateString) => {
+  return new Date(isoDateString).toLocaleTimeString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+};
